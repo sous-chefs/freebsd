f5f9f5d5b6c96ba99ed758c650570b3b47578e54
jtimberman <joshua@opscode.com>
2012-03-22 10:13:21 -0600

    release v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.0.2:
+
+* [COOK-1084] - fix older version building from ports
+
 ## v0.0.1:
 
 * [COOK-1074] - initial release
--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Handles FreeBSD-specific features"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
