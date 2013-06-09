0fc6110c6c9d24762f7851854561f4cdb85c7b33
jtimberman <joshua@opscode.com>
2013-06-09 15:31:49 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Handles FreeBSD-specific features"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 supports "freebsd"
