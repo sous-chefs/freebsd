03a0ceb6eff55e6b040e1e278f5563b85ce1dd90
Tim Smith <tsmith@chef.io>
2017-03-20 12:00:25 -0700

    Update the license string in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'freebsd'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Handles FreeBSD ports setup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
