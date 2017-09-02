f4e0846b4ccb3bfad26e3053467fe30a31b516aa
Tim Smith <tsmith@chef.io>
2017-09-01 21:34:38 -0700

    Add a test recipe for the port_options resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,6 @@
+name 'test'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache-2.0'
+version '0.1.0'
+depends 'freebsd'
---
+++ test/cookbooks/test/recipes/default.rb
@@ -0,0 +1,4 @@
+freebsd_port_options 'php5' do
+  options 'APACHE' => true
+  action :create
+end
