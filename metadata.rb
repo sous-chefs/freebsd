040d732efa2e95aa97fc40622843227fbfa9ea51
Hans Rakers <hans@shoq.com>
2022-08-19 09:14:01 +0200

    Require Chef >= 15.3 for unified_mode

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description       'Handles FreeBSD ports setup'
 version           '1.1.2'
 source_url        'https://github.com/sous-chefs/freebsd'
 issues_url        'https://github.com/sous-chefs/freebsd/issues'
-chef_version      '>= 12.15'
+chef_version      '>= 15.3'
 
 supports 'freebsd'
